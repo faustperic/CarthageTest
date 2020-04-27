dfa86ccd67c74538323d2e54bfccb1289270366d
Stefan Stevanovic <stevanovics31@gmail.com>
2020-04-27 11:27:08 +0200

    carthage files added

---
+++ Carthage/Build/iOS/testCarhtageFramework.framework/Headers/testCarhtageFramework.h
@@ -0,0 +1,19 @@
+//
+//  testCarhtageFramework.h
+//  testCarhtageFramework
+//
+//  Created by Stefan Stevanovic on 4/27/20.
+//  Copyright © 2020 Stefan Stevanovic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for testCarhtageFramework.
+FOUNDATION_EXPORT double testCarhtageFrameworkVersionNumber;
+
+//! Project version string for testCarhtageFramework.
+FOUNDATION_EXPORT const unsigned char testCarhtageFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testCarhtageFramework/PublicHeader.h>
+
+
