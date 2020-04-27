3629390027af661a3db6c9713e26f11ac679a2a8
Stefan Stevanovic <stevanovics31@gmail.com>
2020-04-27 10:17:00 +0200

    shared schemas change

---
+++ CarthategeExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarthategeExample
+//
+//  Created by Stefan Stevanovic on 4/27/20.
+//  Copyright © 2020 Stefan Stevanovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
